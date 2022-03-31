98e6c2a369f878a244afe3193439cb0f7e52fe22
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2022-03-31 11:44:31 +0800

    将使用圆形改成使用图片

--- dev_src/index.js
+++ dev_src/index.js
@@ -1,4 +1,4 @@
-import { Application, Graphics } from 'pixi.js';
+import { Application, Graphics, Loader, Sprite } from 'pixi.js';
 import { io } from "./lib/socket.io.esm.min.js";
 
 function setState(text) {
@@ -15,11 +15,26 @@ function draw(x, y) {
 setState('连接服务器...');
 const socket = io();
 
+var loader = new Loader();
+var my_cursor = new Sprite();
+var id = '';
+var cursors = {};
+
 socket.emit('auth', {
     name: prompt('请输入你的名字'),
 });
-socket.on('authBack', () => {
+socket.on('authBack', ({ current_cursor }) => {
     setState('连接成功');
+    id = socket.id;
+    // loader
+    //     .add('cursor', './img/cursor_' + current_cursor + '.png')
+    //     .load(() => {
+    //         let cursor_texture = loader.resources.cursor.texture;
+    //         my_cursor = new Sprite(cursor_texture);
+    //         app.stage.addChild(my_cursor);
+    //         cursors[id] = my_cursor;
+    //     });
+
 });
 
 // 初始化画板
@@ -53,12 +68,42 @@ app.view.onmousemove = (e) => {
     socket.emit('mouseMove', {
         x,
         y,
+        id,
     });
 }
-socket.on('mouseMove', ({ x, y }) => {
-    draw(x, y);
+socket.on('mouseMove', ({ x, y, id }) => {
+    // draw(x, y);
+    // my_cursor.x = x;
+    // my_cursor.y = y;
+    cursors[id].x = x;
+    cursors[id].y = y;
 });
-socket.on('playerJoin', ({ name }) => {
+socket.on('playerJoin', ({ name, tid, tcurrent_cursor, cursor_list }) => {
+    // console.log(tid);
+    console.log(tcurrent_cursor);
+    if (tid == id) {
+        cursors = cursor_list;
+        for (const key in cursors) {
+            if (Object.hasOwnProperty.call(cursors, key)) {
+                const e = cursors[key];
+                app.stage.addChild(e);
+            }
+        }
+    }
+    loader
+        .add(tcurrent_cursor, './img/cursor_' + tcurrent_cursor + '.png')
+        .load(() => {
+            cursors[tid] = new Sprite(loader.resources[tcurrent_cursor].texture);
+            console.log(cursors);
+            app.stage.addChild(cursors[tid]);
+        });
+    // }
+    setInterval(() => {
+        socket.emit("cursorFetchBack", {
+            sid: id,
+            scursor: cursors[id]
+        });
+    }, 5000);
     setState(`${name}加入了房间`);
 });
 socket.on('playerLeave', ({ name }) => {
--- main.js
+++ main.js
@@ -21,24 +21,44 @@ var cursors = [
     'pink',
     'purple',
 ];
+var cursor_list = {};
+
 io.on('connection', (socket) => {
     var name = '';
     var current_cursor = cursors[member_num];
+    var id = socket.id;
     socket.on('auth', (data) => {
         name = data.name;
         member_num += 1;
-        socket.emit('authBack', true);
-        socket.emit('playerJoin', {
+        current_cursor = cursors[member_num];
+        console.log('===');
+        console.log(current_cursor);
+        console.log(member_num);
+        console.log(cursors[member_num]);
+        socket.emit('authBack', {
+            member_num,
+            current_cursor,
             name,
+            id
+        });
+        io.emit('playerJoin', {
+            name,
+            tid: id,
+            tcurrent_cursor: current_cursor,
+            cursor_list
         });
     });
     socket.on('mouseMove', (data) => {
         io.emit('mouseMove', data);
     });
+    socket.on('cursorFetchBack', ({ sid, scursor }) => {
+        cursor_list[sid] = scursor;
+    });
     socket.on('disconnect', () => {
-        member_num -= 1;
-        socket.emit('playerLeave', {
+        // member_num -= 1;
+        io.emit('playerLeave', {
             name,
+            id
         });
     });
 });
